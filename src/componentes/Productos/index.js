e2956bcfde7f213516dca299dc72a8ab415c226c
Valenargaezceballos02 <valentina.argaez100@pascualbravo.edu.co>
2024-05-11 14:54:01 -0500

    Productos

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -1,9 +1,138 @@
 import React from 'react'
+import IMG from "../../images/img01.jpg";
 
 export const ProductosLista = () => {
   return (
-    <div>
-        <h1>Productos Lista</h1>
-    </div>
+    <>
+      <h1 className="title">PRODUCTOS</h1>
+      <div className="productos">
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto_footer">
+            <h1> Title</h1>
+            <p> Categoria </p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">
+              Añadir al carrito
+            </button>
+            <div>
+              <a href="#" className="btn">Vista</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto_footer">
+            <h1> Title</h1>
+            <p> Categoria </p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">
+              Añadir al carrito
+            </button>
+            <div>
+              <a href="#" className="btn">Vista</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto_footer">
+            <h1> Title</h1>
+            <p> Categoria </p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">
+              Añadir al carrito
+            </button>
+            <div>
+              <a href="#" className="btn">Vista</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto_footer">
+            <h1> Title</h1>
+            <p> Categoria </p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">
+              Añadir al carrito
+            </button>
+            <div>
+              <a href="#" className="btn">Vista</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto_footer">
+            <h1> Title</h1>
+            <p> Categoria </p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">
+              Añadir al carrito
+            </button>
+            <div>
+              <a href="#" className="btn">Vista</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="producto">
+          <a href="#">
+            <div className="producto__img">
+              <img src={IMG} alt="" />
+            </div>
+          </a>
+          <div className="producto_footer">
+            <h1> Title</h1>
+            <p> Categoria </p>
+            <p className="price">$320</p>
+          </div>
+          <div className="buttom">
+            <button className="btn">
+              Añadir al carrito
+            </button>
+            <div>
+              <a href="#" className="btn">Vista</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
