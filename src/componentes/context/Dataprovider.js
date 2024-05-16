ebf9eced463e42d964d374eff57f697d1d9b1b5d
Valenargaezceballos02 <valentina.argaez100@pascualbravo.edu.co>
2024-05-16 11:06:38 -0500

    Retroceder

--- src/componentes/context/Dataprovider.js
+++ src/componentes/context/Dataprovider.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, createContext } from "react";
+<<<<<<< HEAD
+import Data from './Data.js'
+=======
 import Data from "../../Data.js"
+>>>>>>> 4687538dbf1db5acba24a5ca1129ef33d9e8a62f
 export const DataContext = createContext();
 
 
@@ -17,8 +21,13 @@ export const DataProvider = (props) => {
     }
 
     return(
+<<<<<<< HEAD
+        <DataContext.Provider value = {value}>
+         {props.children}
+=======
         <DataContext.Provider value={value}>
             {props.children}
+>>>>>>> 4687538dbf1db5acba24a5ca1129ef33d9e8a62f
         </DataContext.Provider>
     )
 }
