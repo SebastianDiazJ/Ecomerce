8f0daf7b8c6fce33c0d2ae71406054e609a3c972
Valenargaezceballos02 <valentina.argaez100@pascualbravo.edu.co>
2024-05-16 11:15:41 -0500

    Retrocedimiento

--- src/componentes/Productos/Paginas.js
+++ src/componentes/Productos/Paginas.js
@@ -4,6 +4,18 @@ import { Routes, Route } from "react-router-dom";
 import { Inicio } from "../Inicio";
 import { ProductosLista } from "./index";
 
+<<<<<<< HEAD
+export const Paginas = () => (
+  <section>
+    <Router> 
+      <Routes>
+        <Route element={<Inicio />} /> 
+        <Route path="/productos" element={<ProductosLista />} />
+      </Routes>
+    </Router>
+  </section>
+);
+=======
 export const Paginas = () => {
     return (
         <section>
@@ -14,3 +26,4 @@ export const Paginas = () => {
         </section>
     );
 };
+>>>>>>> 4687538dbf1db5acba24a5ca1129ef33d9e8a62f
