1eaf7f8898d6acb3e81a220f753bebde24e45f35
Sebastian Diaz Jimenez <129464465+SebastianDiazJ@users.noreply.github.com>
2024-05-17 10:02:33 -0500

    agrego 5 productos con su estilo y css ya organizado sin errores

--- src/componentes/Productos/index.js
+++ src/componentes/Productos/index.js
@@ -6,8 +6,29 @@ export const ProductosLista =()=>{
     <>
     <h1 className="title">PRODUCTOS</h1>
     <div className="productos">
-      <a href="#">
       <div className="producto">
+      <a href="#">
+      <div className="producto__img">
+      <img src={IMG} alt=""/>
+      </div>
+      </a>
+      <div className="producto__footer">
+        <h1> Title </h1>
+        <p>Categoria</p>
+        <p className="price">$320</p>
+      </div>
+      <div className="buttom">
+        <button className="btn">
+          añadir al carrito
+        </button>
+        <div>
+              <a href="#" className="btn">Vista</a>
+        </div>
+      </div>
+      </div>
+      <div className="producto">
+      <a href="#">
+      <div className="producto__img">
       <img src={IMG} alt=""/>
       </div>
       </a>
@@ -24,6 +45,69 @@ export const ProductosLista =()=>{
               <a href="#" className="btn">Vista</a>
         </div>
         
+      </div>
+      </div>
+      <div className="producto">
+      <a href="#">
+      <div className="producto__img">
+      <img src={IMG} alt=""/>
+      </div>
+      </a>
+      <div className="producto__footer">
+        <h1> Title </h1>
+        <p>Categoria</p>
+        <p className="price">$320</p>
+      </div>
+      <div className="buttom">
+        <button className="btn">
+          añadir al carrito
+        </button>
+        <div>
+              <a href="#" className="btn">Vista</a>
+        </div>
+        
+      </div>
+      </div>
+      <div className="producto">
+      <a href="#">
+      <div className="producto__img">
+      <img src={IMG} alt=""/>
+      </div>
+      </a>
+      <div className="producto__footer">
+        <h1> Title </h1>
+        <p>Categoria</p>
+        <p className="price">$320</p>
+      </div>
+      <div className="buttom">
+        <button className="btn">
+          añadir al carrito
+        </button>
+        <div>
+              <a href="#" className="btn">Vista</a>
+        </div>  
+      </div>
+      </div>
+      <div className="producto">
+      <a href="#">
+      <div className="producto__img">
+      <img src={IMG} alt=""/>
+      </div>
+      </a>
+      <div className="producto__footer">
+        <h1> Title </h1>
+        <p>Categoria</p>
+        <p className="price">$320</p>
+      </div>
+      <div className="buttom">
+        <button className="btn">
+          añadir al carrito
+        </button>
+        <div>
+              <a href="#" className="btn">Vista</a>
+        </div>
+        
+      </div>
       </div>
     </div>
     </>
