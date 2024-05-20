108f8c3d92f09aead12d72e0c56b9f465c5ec974
ELXURITA02 <129228078+ELXURITA02@users.noreply.github.com>
2024-05-19 20:38:41 -0500

    Carrito

--- src/componentes/Carrito/index.js
+++ src/componentes/Carrito/index.js
@@ -1,22 +1,34 @@
-import React from "react"
-import  Card  from "../../images/img03.jpg"
-
+import React from 'react'
+import Card from '../../images/img03.jpg'
 export const Carrito = () => {
-    return(
-        <div className="carrito">
-            <div className="carrito">
-                <div className="carrito__close">
-                    <box-icon name = "x"></box-icon>
+  return (
+    <div className="carrito show">
+        <div className="carrito show">
+            <div className="carrito__close">
+                <box-icon name = "x"></box-icon>
+            </div>
+            <h2>Su carrito</h2>
+            <div className="carrito__center">
+
+
+                <div className = "carrito__item">
+                    <img src={Card} alt=""/>
+                    <div>title</div>
+                    <p className = "price">$345</p>
                 </div>
-                <h2>Su carrito</h2>
-                <div className="carrito__center">
-
-
-                    <div className="carrito__center">
-                        <img src="{Card}" alt="" />
-                    </div>
+                <div>
+                    <box-icon Name ="up-arrow" type ="solid" ></box-icon>
+                    <p className = "cantidad" >1</p>
+                    <box-icon name = "down-arrow" type = "solid"></box-icon>
+                </div>
+                <div className="remoce__item">
+                    <box-icon name="trash" ></box-icon>
                 </div>
             </div>
+            <div className="carrito__footer" ></div>
+            <h3>Total : 2344</h3>
+            <button className="btn">Payment </button>
         </div>
-    )
+    </div>
+  )
 }
