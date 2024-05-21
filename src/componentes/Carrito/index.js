c68430a80d1a98029e4a0920f5f22f0fe9323710
Sebastian Diaz Jimenez <129464465+SebastianDiazJ@users.noreply.github.com>
2024-05-21 13:31:45 -0500

    Estilo de carrito completo con responsive

--- src/componentes/Carrito/index.js
+++ src/componentes/Carrito/index.js
@@ -12,7 +12,7 @@ export const Carrito = () => {
                 <div className="carrito__item">
                     <img src={Card} alt=""/>
                     <div>
-                        <h3>title</h3>
+                        <h3>Nike LD Waffle Sacai Black Nylon</h3>
                         <p className="price">$345</p>
                     </div>
                     <div>
@@ -24,6 +24,53 @@ export const Carrito = () => {
                         <box-icon name="trash"></box-icon>
                     </div>
                 </div>
+                <div className="carrito__item">
+                    <img src={Card} alt=""/>
+                    <div>
+                        <h3>Nike LD Waffle Sacai Black Nylon</h3>
+                        <p className="price">$345</p>
+                    </div>
+                    <div>
+                        <box-icon Name="up-arrow" type="solid"></box-icon>
+                        <p className="cantidad">1</p>
+                        <box-icon Name="down-arrow" type="solid"></box-icon>
+                    </div>
+                    <div className="remove__item">
+                        <box-icon name="trash"></box-icon>
+                    </div>
+                </div>
+                <div className="carrito__item">
+                    <img src={Card} alt=""/>
+                    <div>
+                        <h3>Nike LD Waffle Sacai Black Nylon</h3>
+                        <p className="price">$347</p>
+                    </div>
+                    <div>
+                        <box-icon Name="up-arrow" type="solid"></box-icon>
+                        <p className="cantidad">1</p>
+                        <box-icon Name="down-arrow" type="solid"></box-icon>
+                    </div>
+                    <div className="remove__item">
+                        <box-icon name="trash"></box-icon>
+                    </div>
+                </div>
+                <div className="carrito__item">
+                    <img src={Card} alt=""/>
+                    <div>
+                        <h3>Nike LD Waffle Sacai Black Nylon</h3>
+                        <p className="price">$345</p>
+                    </div>
+                    <div>
+                        <box-icon Name="up-arrow" type="solid"></box-icon>
+                        <p className="cantidad">1</p>
+                        <box-icon Name="down-arrow" type="solid"></box-icon>
+                    </div>
+                    <div className="remove__item">
+                        <box-icon name="trash"></box-icon>
+                    </div>
+                </div>
+
+
             </div>
             <div className="carrito__footer">
                 <h3>Total:2334</h3>
